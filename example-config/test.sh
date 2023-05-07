b45ba984da2a0b1f179b702cf3d8c18fa70d7a43
Theodore Alenas <dimakopt732@gmail.com>
2023-05-07 22:48:18 +0300

    alc-talk is fully gone. Not tested.

--- example-config/test.sh
+++ example-config/test.sh
@@ -11,95 +11,13 @@ fn_pigh() {
   esac
 }
 
-fn=t_bat+key:"$fn"
-fn_t_bat() {
-  case "$act" in
-    script) printf 'alm-battery -c %s\n' "$1" ;;
-    color) printf "sed 's/-c/\\nattention\\n-c/'\n" ;;
-    arg1) printf "recommend\n0\n1\n20\n40\n80\n90\n99\n100\n" ;;
-    descr)
-      cat <<EOM
-Try out different 'capacity' levels of the battery.
-
-> alm-battery --help
-$(alm-battery --help)
-EOM
-    ;;
-  esac
-}
-
-fn=t_fas+key+is-f:"$fn"
-fn_t_fas() {
-  case "$act" in
-    script) printf "%s\n" 'alc-test fast' ;;
-    color) printf "sed '%s'\n" 's/test/\nattention\ntest\nnormal\n/' ;;
-    descr)
-      cat <<EOM
-> alc-test --help
-$(alc-test --help)
-
-Run tests made for the superficial elements I make.
-Adapters aren't tested typically, but there are a few scripts with
-logic in them. Also some can cause a big mess if they don't work
-right. As of now, April 2023, they're in ~/code/sys/tests and hard
-linked to ~/.local/albin with names like test-*
-
-> grep test -B 2 -A 4 ~/code/sys/Makefile
-$(grep test -B 2 -A 4 ~/code/sys/Makefile)
-EOM
-    ;;
-  esac
-}
-
-fn=t_sys+key:"$fn"
-fn_t_sys() {
-  case "$act" in
-    script) printf 'alc-test\n' ;;
-    descr)
-      cat <<EOM
-> alc-test --help
-$(alc-test --help)
-
-Run tests made for the superficial elements I make.
-Adapters aren't tested typically, but there are a few scripts with
-logic in them. Also some can cause a big mess if they don't work
-right. As of now, April 2023, they're in ~/code/sys/tests and hard
-linked to ~/.local/albin with names like test-*
-
-> grep test -B 2 -A 4 ~/code/sys/Makefile
-$(grep test -B 2 -A 4 ~/code/sys/Makefile)
-EOM
-    ;;
-  esac
-}
-
-fn=t_snp+key:"$fn"
-fn_t_snp() {
-  case "$act" in
-    script) printf 'test-snippet\n' ;;
-    descr)
-      cat <<EOM
-Test alm-snippet (the program with the fewer dependencies).
-It's not at all a proof things work right.
-EOM
-    ;;
-  esac
-}
-
-fn=echo+key:"$fn"
-fn_echo() {
-  case "$act" in
-    script) printf 'echo hi\n' ;;
-    descr) printf 'For testing purposes\n' ;;
-  esac
-}
-
 fn=echh+key:"$fn"
 fn_echh() {
   case "$act" in
-    script) printf 'echo hi\n' ;;
-    color) printf "sed 's/hi/\\nattention\\nhi/'\n" ;;
+    script) printf 'echo %s\n' "$1" ;;
+    arg1) printf "get-line\nWhat should be echoed\n" ;;
     descr) printf 'For testing purposes\n' ;;
+    color) printf "sed '%s'\n" 's/echo/\nsecondary\necho\nnormal\n/' ;;
   esac
 }
 
@@ -108,14 +26,6 @@ fn_eche() {
   case "$act" in
     script) printf "%s\n" '(echo error >&2; exit 1)' ;;
     color) printf "sed '%s'\n" 's/error/\nattention\nerror\nnormal\n/' ;;
-  esac
-}
-
-fn=vars+key+is-v-v:"$fn"
-fn_vars() {
-  case "$act" in
-    script) printf 'alc-memory permanent get-all\n' ;;
-    descr) alc-memory --help ;;
   esac
 }
 
